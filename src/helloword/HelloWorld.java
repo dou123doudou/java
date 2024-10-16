d1c7b7fd9810d233611a041c4edeb2cfd6b7d77f
dou123doudou <dangnhat1010@gmail.com>
2024-10-16 12:52:15 +0700

    init project

---
+++ src/helloword/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloword;
+
+public class HelloWorld {
+ public static void main(String[] args) {
+	 System.out.println("Hello World with Java and ThaoNghi");
+	 		System.out.println("Hello World with Java and ThaoNghi");
+	 	System.out.println("Hello World with Java and ThaoNghi");
+	 		System.out.println("Hello World with Java and ThaoNghi");
+	 System.out.println("Hello World with Java and ThaoNghi");
+
+}
+ 
+}
